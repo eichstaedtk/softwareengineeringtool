a77dc091468dc8878a82d4ff919d69d71018c28e
Konrad Eichstädt <eichstak@th-brandenburg.de>
2024-09-12 21:23:50 +0200

    Adjust maven gihub action

---
+++ src/test/java/de/eichstaedt/engineering/application/IndexPageTest.java
@@ -0,0 +1,29 @@
+package de.eichstaedt.engineering.application;
+
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by konrad.eichstaedt@gmx.de on 12.09.24.
+ */
+@QuarkusTest
+public class IndexPageTest {
+
+  @TestHTTPResource("index.html")
+  URL url;
+
+  @Test
+  public void testIndexHtml() throws IOException {
+    try (InputStream in = url.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      Assertions.assertTrue(contents.contains("<h1>Welcome to Pure Software-Engineering</h1>"));
+    }
+  }
+
+}
