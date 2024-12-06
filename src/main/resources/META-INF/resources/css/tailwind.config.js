4f503bf429a7b9fe9f46176fb5fc87c9fe7dd78a
Konrad Eichstädt <eichstak@th-brandenburg.de>
2024-12-06 17:19:42 +0100

    Add Tailwind as css Framework for evaluation

---
+++ src/main/resources/META-INF/resources/css/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../../../templates/**/*.html',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
